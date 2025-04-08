6c1fa5a9603cafb09d06841b57c36fd99ae91c61
정재웅 <jweidon0821@gmail.com>
2025-04-08 12:06:57 +0900

    deploy(server): HTTPS 설정 적용

--- client/src/api/upbit.ts
+++ client/src/api/upbit.ts
@@ -4,6 +4,7 @@ import { apiClient } from './config';
 export const upbitApi = {
     getAccounts: async (): Promise<UpbitAccount[]> => {
         const response = await apiClient.get<UpbitAccount[]>('/upbit/accounts');
+        console.log('업비트 계좌 정보 응답:', response.data);
         return response.data;
     }
 }; 
--- server/src/main/java/com/everbit/everbit/client/UpbitClient.java
+++ server/src/main/java/com/everbit/everbit/client/UpbitClient.java
@@ -3,7 +3,6 @@ package com.everbit.everbit.client;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.everbit.everbit.config.UpbitConfig;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,8 +17,6 @@ import java.net.URI;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 /**
