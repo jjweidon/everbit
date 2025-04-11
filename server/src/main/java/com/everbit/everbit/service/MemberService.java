fc9d88f772472cda89c619ab7d8881bf36ca6209
정재웅 <jweidon0821@gmail.com>
2025-04-11 20:15:56 +0900

    feat(server): 이미 존재하는 사용자는 로그인 처리

--- server/src/main/java/com/everbit/everbit/service/MemberService.java
+++ server/src/main/java/com/everbit/everbit/service/MemberService.java
@@ -7,9 +7,13 @@ import com.everbit.everbit.oauth2.KakaoResponse;
 import com.everbit.everbit.oauth2.OAuth2Response;
 import com.everbit.everbit.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -18,9 +22,16 @@ public class MemberService {
     public Member createMember(OAuth2User oAuth2Member) {
         OAuth2Response oAuth2Response = new KakaoResponse(oAuth2Member.getAttributes());
         String username = oAuth2Response.getName()+" "+oAuth2Response.getProviderId();
-        if (memberRepository.findByUsername(username).isPresent()) {
-            throw MemberException.alreadyExists(username);
+        
+        // 이미 존재하는 사용자는 새로 생성하지 않고 기존 정보 반환
+        Optional<Member> existingMember = memberRepository.findByUsername(username);
+        if (existingMember.isPresent()) {
+            log.info("기존 사용자 로그인: {}", username);
+            return existingMember.get();
         }
+        
+        // 새로운 사용자 생성
+        log.info("새로운 사용자 등록: {}", username);
         Member member = Member.builder()
                 .username(username)
                 .image(oAuth2Response.getImage())
