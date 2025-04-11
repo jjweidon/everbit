1228d0148ad2bc3044a03c8fc75f1639187f05a0
정재웅 <jweidon0821@gmail.com>
2025-04-11 17:09:03 +0900

    fix(client): 서버의 카카오 로그인 리다이렉션에 대한 CORS 오류 수정

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -12,14 +12,7 @@ export const apiClient = axios.create({
 
 // 응답 인터셉터 추가
 apiClient.interceptors.response.use(
-    (response) => {
-        if (response.data.data) {
-            console.log('카카오 로그인 URL로 리다이렉트:', response.data.data);
-            // 여기서 response.data.data가 http://로 시작하는 URL일 수 있습니다
-            window.location.href = response.data.data;
-        }
-        return response;
-    },
+    (response) => response,
     (error) => {
         // 에러 처리
         console.error('API Error:', error);
