241321dd8a44a2250b6c2ebfc013950a1c34d466
정재웅 <jweidon0821@gmail.com>
2025-04-16 13:55:22 +0900

    fix(client): vercel - next 설정 충돌 수정

--- client/next.config.js
+++ client/next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  optimizeFonts: true,
-  images: {
-    domains: ['k.kakaocdn.net', 'lh3.googleusercontent.com'],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
-      },
-    ];
-  },
+  trailingSlash: true,
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
