ab7ca7001655144149b2f29a17a77ae7550a0e2d
정재웅 <jweidon0821@gmail.com>
2025-04-16 14:40:06 +0900

    fix(client): vercel.json output directory 설정

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   trailingSlash: true,
-  distDir: '.next',
-  generateBuildId: async () => {
-    return 'build-' + Date.now();
-  }
 };
 
 module.exports = nextConfig;
