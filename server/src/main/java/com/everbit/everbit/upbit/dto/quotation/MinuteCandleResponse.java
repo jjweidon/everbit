a9bb7879a7ad03407a7879d71ba190097b79de8d
정재웅 <jweidon0821@gmail.com>
2025-07-22 15:50:46 +0900

    fix(server): 시세 캔들 조회 API 응답 OffsetDateTime -> LocalDateTime 형식 수정

--- server/src/main/java/com/everbit/everbit/upbit/dto/quotation/MinuteCandleResponse.java
+++ server/src/main/java/com/everbit/everbit/upbit/dto/quotation/MinuteCandleResponse.java
@@ -2,13 +2,13 @@ package com.everbit.everbit.upbit.dto.quotation;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import java.time.OffsetDateTime;
+import java.time.LocalDateTime;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MinuteCandleResponse(
     String market,
-    OffsetDateTime candleDateTimeUtc,
-    OffsetDateTime candleDateTimeKst,
+    LocalDateTime candleDateTimeUtc,
+    LocalDateTime candleDateTimeKst,
     Double openingPrice,
     Double highPrice,
     Double lowPrice,
--- server/src/main/java/com/everbit/everbit/upbit/dto/quotation/WeekCandleResponse.java
+++ server/src/main/java/com/everbit/everbit/upbit/dto/quotation/WeekCandleResponse.java
@@ -2,14 +2,14 @@ package com.everbit.everbit.upbit.dto.quotation;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import java.time.OffsetDateTime;
+import java.time.LocalDateTime;
 import java.time.LocalDate;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record WeekCandleResponse(
     String market,
-    OffsetDateTime candleDateTimeUtc,
-    OffsetDateTime candleDateTimeKst,
+    LocalDateTime candleDateTimeUtc,
+    LocalDateTime candleDateTimeKst,
     Double openingPrice,
     Double highPrice,
     Double lowPrice,
